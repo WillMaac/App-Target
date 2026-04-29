2b6b4c1a02ff90d4c1f7505d90d567b7fa57d97d
WillMaac <anderrwil@gmail.com>
2026-04-29 17:30:30 -0300

    Adiciona o componente Home

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,16 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "./styles";
+import { colors } from "@/theme/colors";
+import { Text, View } from "react-native";
+
+export function HomeHeader(){
+    return(
+        <LinearGradient 
+        colors={[colors.blue[500], colors.blue[800]]}
+        style={styles.container}>
+            <View>
+                <Text style={styles.label}>Total que você possui</Text>
+            </View>
+        </LinearGradient>
+    )
+}
