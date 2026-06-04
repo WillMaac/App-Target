af1d84d6eb05d38ed4605b1d07da408486662e7d
WillMaac <anderrwil@gmail.com>
2026-06-04 13:20:47 -0300

    Adiciona o componente List e sua estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { HomeHeader } from "@/components/Home";
 import {  View } from "react-native";
 import {Target} from "@/components/Target"
+import { List } from "@/components/List";
+import { ImportMetaRegistry } from "expo/build/winter/ImportMetaRegistry";
 
 
 const summary={
@@ -11,11 +13,28 @@ const summary={
 
 const targets = [
     {
+        id: "2",
     name: "Comprar uma cadeira ergonômica",
     percentage: "75%",
-    current: "900,00",
-    target: "1.200,00"
+    current: "R$ 900,00",
+    target: "R$ 1.200,00"
 },
+
+{
+        id: "1",
+    name: "App Watch",
+    percentage: "50%",
+    current: "R$ 580,00",
+    target: "R$ 1.700,00"
+},
+
+{
+        id: "3",
+    name: "Fazer uma viagem para o Rio de Janeiro",
+    percentage: "75%",
+    current: "R$ 1200,00",
+    target: " R$ 3.000,00"
+}
 ]
 
 export default function Index() {
@@ -23,7 +42,15 @@ export default function Index() {
         <View style= {{flex:1}}>
 <HomeHeader data={summary}/>
 
-<Target data={targets[0]}/>   
+<List
+title="Metas"
+data={[]}
+keyExtractor={(item) => item.id}
+renderItem={({item}) => <Target data={item}/>}
+emptyMessage="Nenhuma meta. Toque em uma nova meta para criar."
+contentContainerStyle={{paddingHorizontal: 24}}
+
+/>   
         </View>
     )
 }
