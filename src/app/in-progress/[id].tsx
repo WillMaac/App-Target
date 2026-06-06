87ea4ef4ee0ce80823bd6c1a12a8d3d836bffae2
WillMaac <anderrwil@gmail.com>
2026-06-06 16:25:34 -0300

    feat: implementa navegação no componente InProgress

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,18 +1,37 @@
+import { List } from '@/components/List';
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
 import { router, useLocalSearchParams } from 'expo-router';
-import { View, Text, Button } from 'react-native';
-
+import { View } from 'react-native';
+import {Transaction, TransactionProps} from '@/components/Transaction';
+import {TransactionTypes} from "@/utils/TransactionsTypes"
+import { Button } from '@/components/Button';
 const details = {
-    current: "R$ 580, 00",
-    target: "R$ 1790,00",
-    percentage: 25,
-  };
+  current: 'R$ 580, 00',
+  target: 'R$ 1790,00',
+  percentage: 25,
+};
+
+const transactions: TransactionProps[] = [
+  {
+    id: '1',
+    value: 'R$ 20,00',
+    date: '12/04/26',
+    type: TransactionTypes.Output,
+  },
+
+  {
+    id: '2',
+    value: 'R$ 300,00',
+    date: '12/04/26',
+    description: 'CDB de 110% no banco XPTO',
+    type: TransactionTypes.Input,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
 
-  
   return (
     <View style={{ flex: 1, padding: 24, gap: 32 }}>
       <PageHeader
@@ -24,6 +43,20 @@ export default function InProgress() {
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => 
+          <Transaction data={item} onRemove={() => {}} />
+        }
+
+        emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+      />
+      <Button
+      title="Nova transação" 
+      onPress={()=> router.navigate(`/transaction/${params.id}`)}
+      />
     </View>
   );
 }
